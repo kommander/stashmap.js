692a6f23cdc2d9380c71e646e9c7511bdce6ed79
Sebastian Herrlinger <sebastian@formzoo.com>
2013-05-24 18:52:16 +0200

    initial commit

---
+++ test/stashmap.test.js
@@ -0,0 +1,93 @@
+/**
+ * Tests
+ */
+var expect = require('expect.js');
+var sinon = require('sinon');
+
+var Stashmap = require('../');
+
+describe('Stashmap', function(){
+
+  //
+  //
+  describe('#length', function(){
+    //
+    //
+    it('should have correct length', function(){
+      var map = new Stashmap();
+      map.set('a', 1);
+      map.set('b', 2);
+      map.set('c', 3);
+      map.set('d', 4);
+      map.remove('c');
+
+      expect(map.length).to.be(3);
+    });
+
+  });
+
+  //
+  //
+  describe('#get()', function(){
+    //
+    //
+    it('should return correct value for key', function(){
+      var map = new Stashmap();
+      map.set('a', 1);
+      map.set('b', 2);
+      map.set('c', 3);
+      map.set('d', 4);
+      map.remove('c');
+
+      expect(map.get('b')).to.be(2);
+    });
+
+  });
+
+  //
+  //
+  describe('#getAt()', function(){
+    //
+    //
+    it('should return correct element', function(){
+      var map = new Stashmap();
+      map.set('a', 1);
+      map.set('b', 2);
+      map.set('c', 3);
+      map.set('d', 4);
+      map.remove('c');
+
+      expect(map.getAt(2)).to.be(4);
+    });
+
+  });
+
+  //
+  //
+  describe('#has()', function(){
+    //
+    //
+    it('should return true/false if a given key exists/not exists in the map', function(){
+      var map = new Stashmap();
+      map.set('a', 1);
+      map.set('b', 2);
+      map.set('c', 3);
+      map.set('d', 4);
+      map.remove('c');
+
+      expect(map.has('c')).to.be(false);
+      expect(map.has('d')).to.be(true);
+    });
+
+  });
+
+  //
+  //
+  describe('#forEach()', function(){
+    //
+    //
+    it('should iterate over key/values');
+
+  });
+
+});
